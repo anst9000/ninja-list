336bd88c87b0248843ba085f81bd8656b388e4f3
Anders Strömberg <coltlaboy@yahoo.se>
2021-04-24 21:34:14 +0200

    initial

---
+++ pages/about.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          voluptatem exercitationem error deserunt sit totam mollitia temporibus
+          fugiat, earum maiores odit quae autem, aut iste!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+          voluptatem exercitationem error deserunt sit totam mollitia temporibus
+          fugiat, earum maiores odit quae autem, aut iste!
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ pages/ninjas/test.js
@@ -0,0 +1,9 @@
+const Test = () => {
+  return (
+    <div className="">
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default Test;
